861565a81cb9638e3f8272862a1d31d149e1a512
capimaker <capimaker@gmail.com>
2025-05-15 10:52:18 +0200

    tercera prueba

---
+++ main.js
@@ -0,0 +1,39 @@
+
+
+/*const startButton = document.getElementById("start-btn");
+const nextButton = document.getElementById("next-btn");
+const questionContainer = document.getElementById("quiz-container");
+const questionElement = document.getElementById("question");
+const answerButtonElement = document.getElementById("answer-buttons");
+
+
+
+ const API_URL ="https://opentdb.com/api.php?amount=10&category=9&type=multiple";
+console.log(API_URL);
+
+const quiz = async (e) =>{
+e.preventDedault();
+try{
+  const quizz = quest.value;
+  const res = await axios.get(API_URL)
+  console.log(res)
+  const quizzy = res.data.results;
+    showQuiz(quizzy);
+}catch(error){
+console.log(error);
+}
+};
+
+const showQuiz = (questions) => {
+  questionContainer.innerHTML = "";
+  quizzy.forEach((questions) => {
+    questionContainer.innerHTML += `
+    `;
+  });
+};
+
+startButton.addEventListener("submit" quiz);
+//console.log((Response.data));
+*/
+
+
